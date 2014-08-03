ff1b0b6f9addd0a623de7d81e4284be7d3964bc8
Daniel Andersen <daniel.andersen@alexandra.dk>
2014-08-03 20:28:54 +0200

    Initial Commit

---
+++ Clever/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Clever
+//
+//  Created by Daniel Andersen on 03/08/14.
+//  Copyright (c) 2014 Trolls Ahead. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
